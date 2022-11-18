3bf42cad9eaf71c3ab90483938b16ed4f79c520b
guilherme barbosa lima <guilhermebali@gmail.com>
2022-11-17 21:39:52 -0300

    implement rules and functions.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,58 @@
-console.log('carro');
+function cleanInput(){
+    document.querySelector("#valuePrime").value = '';
+    document.querySelector("#valueSecond").value = '';   
+    document.querySelector('#valueResult').value = '';
+}
+
+function capValuesSum()
+{    
+    let valuePrime = document.querySelector("#valuePrime").value;
+    let valueSecond = document.querySelector("#valueSecond").value;
+
+    if (valuePrime == '' || valueSecond == ''){
+        alert("Não deixe os campos vazios!!!");
+    } else {
+        document.querySelector('#valueResult').value = parseFloat(valuePrime) + parseFloat(valueSecond);
+    }       
+}
+
+function capValuesDecrease()
+{    
+    let valuePrime = document.querySelector("#valuePrime").value;
+    let valueSecond = document.querySelector("#valueSecond").value;
+   
+    if (valuePrime == '' || valueSecond == ''){
+        alert("Não deixe os campos vazios!!!");
+    } else {
+        document.querySelector('#valueResult').value = parseFloat(valuePrime) - parseFloat(valueSecond);
+    }
+}
+
+function capValuesMultiply()
+{    
+    let valuePrime = document.querySelector("#valuePrime").value;
+    let valueSecond = document.querySelector("#valueSecond").value;
+   
+    if (valuePrime == '' || valueSecond == ''){
+        alert("Não deixe os campos vazios!!!");
+    } else {
+        document.querySelector('#valueResult').value = parseFloat(valuePrime) * parseFloat(valueSecond);
+    }
+}
+
+function capValuesShare()
+{    
+    let valuePrime = document.querySelector("#valuePrime").value;
+    let valueSecond = document.querySelector("#valueSecond").value;
+
+    if (valueSecond > 0){
+        if (valuePrime == '' || valueSecond == ''){
+            alert("Não deixe os campos vazios!!!");
+        } else {
+            document.querySelector('#valueResult').value = parseFloat(valuePrime) / parseFloat(valueSecond);
+        }
+    } else {        
+        document.querySelector('#valueResult').value = 'ERRO';
+    }  
+    
+}
